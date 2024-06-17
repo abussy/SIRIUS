743f4e856f60d3d2a1a3671386c45db3556f9675
Augustin Bussy <augustin.bussy@cscs.ch>
2024-06-17 09:55:09 +0200

    cleanup

--- julia_module/dftk_test.jl
+++ julia_module/dftk_test.jl
@@ -21,22 +21,18 @@ basis = SiriusBasis(model; Ecut=30, kgrid=[4, 4, 4])
 scfres = self_consistent_field(basis; ρ=ρ0, tol=1.0e-10, maxiter=25, mixing=SimpleMixing())
 #@show scfres.eigenvalues[1] .- scfres.eigenvalues[1][1]
 #@show scfres.occupation[1]
-@show scfres.energies.PspLocal
-@show scfres.energies.Kinetic
-@show scfres.energies.Xc
+@show scfres.energies.total
 
 
 #DFTK
 println("\n\n\n")
-C = ElementPsp(:C; psp=load_psp("./C_molsim.upf"))                                                 
+C = ElementPsp(:C; psp=load_psp("./C_molsim.upf"; rcut=10.0))
 atoms     = [C, C]                                                                                   
 model = model_PBE(lattice, atoms, positions)#, temperature=0.000001)
 
 basis_dftk = PlaneWaveBasis(model; Ecut=30, kgrid=[4, 4, 4])
 ρ0 = guess_density(basis_dftk)                                                                   
 scfres = self_consistent_field(basis_dftk; ρ=ρ0, tol=1e-10, maxiter=35, mixing=SimpleMixing())
-#@show scfres.eigenvalues[1] .- scfres.eigenvalues[1][1]
-#@show scfres.occupation[1]
-@show scfres.energies
+@show scfres.energies.total
 
 # Julia extensions => make SIRIUS an extension to DFTK, such that onlu used if SIRIUS.jl is imported
